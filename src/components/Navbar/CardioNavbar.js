602eaf5bf4eff18bc82b57433ff2c38a8080bc22
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-06-23 21:19:25 -0600

    register

--- src/components/Navbar/CardioNavbar.js
+++ src/components/Navbar/CardioNavbar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 //import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 //import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 //import Button from 'react-bootstrap/Button';
 import CardioOffcanvas from './CardioOffcanvas';
 import cardioLogo from '../../assets/images/logoCardio1.png'
@@ -11,6 +11,39 @@ import { colorMorado } from '../../assets/constants';
 
 const CardioNavbar = () => {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
 
   const toggleOffcanvas = ()=>{
     setShowOffcanvas(prev=>!prev);
